2fecbcfcb98bf4d0dcb0180d1df6cd6218353a36
Dusan Stoiljkovic <dusanstoiljkovic02@gmail.com>
2026-05-26 13:03:56 +0200

    Remove NavbarCustomer and Create appointment components; add LoginPage component for user authentication

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,49 +1,14 @@
-import Create from '@/views/customers/appointments/Create.vue'
-import CreateCustomerView from '@/views/customers/CreateCustomerView.vue'
-import CustomerDetailsView from '@/views/customers/CustomerDetailsView.vue'
-import CustomersView from '@/views/customers/CustomersView.vue'
-import EditCustomerView from '@/views/customers/EditCustomerView.vue'
-import HomeCustomer from '@/views/customers/HomeCustomer.vue'
-import LandingPage from '@/views/customers/LandingPage.vue'
+import LoginPage from '@/views/auth/LoginPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'landing-page',
-      component: LandingPage
+      path: '/login',
+      name: 'login-page',
+      component: LoginPage,
     },
-    {
-      path: '/customers/appointments/create',
-      name: 'create-appointment',
-      component: Create
-    },
-    {
-      path: '/customers',
-      name: 'customers',
-      component: CustomersView,
-      meta: {
-        requiredAuth: true,
-        roles: ['ADMIN', 'MANAGER', 'RECEPTIONIST'],
-      }
-    },
-    {
-      path: '/customers/create',
-      name: 'create-customer',
-      component: CreateCustomerView
-    },
-    {
-      path: '/customers/:id',
-      name: 'customer-details',
-      component: CustomerDetailsView
-    },
-    {
-      path: '/customers/:id/edit',
-      name: 'edit-customer',
-      component: EditCustomerView,
-    }
   ],
 })
 
